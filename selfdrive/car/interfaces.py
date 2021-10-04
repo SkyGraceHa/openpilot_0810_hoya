21a8653d9436728b53c7baef36eaec654ab9425d
SkyGraceHa <gopyeong@naver.com>
2021-10-05 07:35:33 +0900

    Update interfaces.py

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -114,9 +114,9 @@ class CarInterfaceBase():
   #    events.add(EventName.doorOpen)
   #  if cs_out.seatbeltUnlatched:
   #    events.add(EventName.seatbeltNotLatched)
-  #  if cs_out.gearShifter != GearShifter.drive and (extra_gears is None or
-  #     cs_out.gearShifter not in extra_gears) and cs_out.cruiseState.enabled:
-  #    events.add(EventName.wrongGear)
+    if cs_out.gearShifter != GearShifter.drive and (extra_gears is None or
+       cs_out.gearShifter not in extra_gears) and cs_out.cruiseState.enabled:
+      events.add(EventName.wrongGear)
     if cs_out.gearShifter == GearShifter.reverse:
       events.add(EventName.reverseGear)
     if not cs_out.cruiseState.available and cs_out.cruiseState.enabled:
